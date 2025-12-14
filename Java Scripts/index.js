dfd9866970387a5c487adcbb64162a45c9b89cec
Ali Ahmadi <ahmadi1986.ali@gmail.com>
2025-12-14 16:59:39 +0330

    #0002

--- Java Scripts/index.js
+++ Java Scripts/index.js
@@ -1,1 +1,22 @@
-alert(39)
+// var num1 = prompt("inter number1");
+// var num2 = prompt("inter number2");
+
+// var typenumber1=Number (num1);
+// var typenumber2=Number (num2);
+
+// var result=typenumber1*typenumber2;
+
+// alert (result);
+
+
+
+// //
+
+
+// var x = prompt("inter number1");
+// var y = prompt("inter number2");
+
+// alert(Number(x) % Number(y) == 0);
+
+// //
+
