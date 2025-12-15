fbf21749b25c3c05068b38d6ee553f65e90bcf94
Ali Ahmadi <ahmadi1986.ali@gmail.com>
2025-12-15 17:24:42 +0330

    #0003

--- Java Scripts/index.js
+++ Java Scripts/index.js
@@ -8,10 +8,7 @@
 
 // alert (result);
 
-
-
 // //
-
 
 // var x = prompt("inter number1");
 // var y = prompt("inter number2");
@@ -20,3 +17,61 @@
 
 // //
 
+// //
+
+// var x =prompt("عدد رو وارد کنید")
+// var xtype=Number(x)
+
+// if (xtype%2==0) {
+//     alert("زوج")
+// } else {
+//     alert("فرد")
+// }
+
+//
+
+// var wight = prompt("وزن خود را وارد کنید:");
+// var height = prompt("قد خود را وارد کنید:");
+
+// var wightType = Number(wight);
+// var heightType = Number(height);
+
+// var resultheight = heightType * heightType;
+// var BMI = wightType / resultheight;
+
+// if (BMI > 30) {
+//   alert("چاقی");
+// } else {
+//   alert("خوب");
+// }
+
+//
+
+// var x = prompt("سنتو وارد کن:");
+// var xType = Number(x);
+
+// if (xType >= 18) {
+//   alert("سن قانونی داری");
+// } else if (xType >= 12) {
+//   alert("نوجوانی");
+// } else if (xType < 12) {
+//   alert("کودکی");
+// } else {
+//   alert("خردسالی");
+// }
+
+
+//
+
+// var weight = prompt("سن خود را وارد کنید:");
+// var weightType = Number(weight);
+
+// if (weightType < 25) {
+//   alert("ایده آل");
+// } else if (weightType < 30) {
+//   alert("اضافه وزن");
+// } else if (weightType < 40) {
+//   alert("چاق");
+// } else {
+//   alert("چاقی مفرط");
+// }
