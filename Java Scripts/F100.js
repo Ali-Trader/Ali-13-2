ccafa79d75e94f3a70cfb9aec7d3270df3ad83f8
Ali Ahmadi <ahmadi1986.ali@gmail.com>
2025-12-27 17:05:14 +0330

    #10-06

---
+++ Java Scripts/F100.js
@@ -0,0 +1,32 @@
+
+// تمرین if
+
+// var teuori= prompt("نمره تئوری خودت رو وارد کن:");
+// var amali= prompt("نمره عملی خودت رو وارد کن");
+
+// if (teuori>=10 && amali>=10) {
+//     alert("دمت گرم قبولی")
+// }
+
+// else if(teuori>=8 || amali>=12){
+//     alert(" با این حال بازم دمت گرم قبولی")
+// }
+
+
+// else {
+//     alert("ای بابا مردود شدی که")
+// }
+
+// 
+
+// تمرین ماشین حساب
+
+var number1=prompt("یک عددی رو وارد کن");
+var number2=prompt("عدد  دیگه ای هم وارد کن");
+
+var sum=(number1+number2);
+var menha=(number1-number2);
+var taghsim=(number1/number2);
+var zarb=(number1*number2);
+
+alert("sum")
