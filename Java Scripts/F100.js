87bcdb276894ae7d2e7281dcd3c871c27a3a9700
Ali Ahmadi <ahmadi1986.ali@gmail.com>
2025-12-29 15:45:59 +0330

    #10-08

--- Java Scripts/F100.js
+++ Java Scripts/F100.js
@@ -1,4 +1,3 @@
-
 // تمرین if
 
 // var teuori= prompt("نمره تئوری خودت رو وارد کن:");
@@ -12,21 +11,64 @@
 //     alert(" با این حال بازم دمت گرم قبولی")
 // }
 
-
 // else {
 //     alert("ای بابا مردود شدی که")
 // }
 
-// 
+//
 
 // تمرین ماشین حساب
 
-var number1=prompt("یک عددی رو وارد کن");
-var number2=prompt("عدد  دیگه ای هم وارد کن");
+// var number1=prompt("یک عددی رو وارد کن");
+// var number2=prompt("عدد  دیگه ای هم وارد کن");
 
-var sum=(number1+number2);
-var menha=(number1-number2);
-var taghsim=(number1/number2);
-var zarb=(number1*number2);
+// var sum=(number1+number2);
+// var menha=(number1-number2);
+// var taghsim=(number1/number2);
+// var zarb=(number1*number2);
 
-alert("sum")
+// alert("sum")
+
+//
+
+// BMI
+
+// var weight = prompt("لطفا وزن خود را به کیلوگرم وارد کنید:");
+
+// var height = prompt("لطفا قد خود را به سانتیمتر وارد کنید:");
+// var gender = prompt("لطفا جنسیت خودتان را وارد کنید ، آقا یا خانم:");
+
+// var uweight = Number(weight);
+// var uheight = Number(height) / 100;
+
+// var BMI = uweight / uheight ** 2;
+
+// alert(BMI);
+
+// if (gender == "آقا") {
+//   if (BMI >= 18 && BMI < 25) {
+//     alert("وزن شما متناسب است");
+//   } else if (BMI < 18) {
+//     alert("شما کمبود وزن دارید");
+//   } else {
+//     alert("شما دارای اضافه وزن هستید");
+//   }
+// } else if (gender == "خانم") {
+//   if (BMI >= 20 && BMI < 28) {
+//     alert("وزن شما متناسب است");
+//   } else if (BMI < 20) {
+//     alert("شما کمبود وزن دارید");
+//   } else {
+//     alert("شما دارای اضافه وزن هستید");
+//   }
+// }
+
+// 
+
+// 
+
+function nam() {
+    alert("salam ali");
+}
+
+nam();
